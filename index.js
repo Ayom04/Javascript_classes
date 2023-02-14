1aa9d8a2294a09081601d3764ed67d9f21cedfbf
Ayom04 <abdullahayomide04@gmail.com>
2023-02-14 14:57:56 +0100

    vriables

---
+++ index.js
@@ -0,0 +1,27 @@
+let currentYear = 2023
+console.log(currentYear)
+
+let previousYear = currentYear - 1
+console.log(previousYear)
+
+let nextYear = currentYear + 1
+console.log(nextYear)
+
+console.log(currentYear - previousYear)
+console.log(nextYear - previousYear)
+
+let firstNumber = 5
+let secondNumber = 3
+
+let sum = firstNumber + secondNumber 
+let subtraction = firstNumber - secondNumber 
+let multiplication = firstNumber * secondNumber 
+let division = firstNumber / secondNumber 
+let modulus = firstNumber % secondNumber 
+console.log(sum)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+console.log(modulus)
+
+console.log(5)
