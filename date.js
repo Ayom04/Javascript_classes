3ed4b07b2954129f8bdca2966d679a8ea2cf96f1
Ayom04 <abdullahayomide04@gmail.com>
2023-03-08 12:53:05 +0100

    date

--- date.js
+++ date.js
@@ -1,11 +1,57 @@
-const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
-const [hour, minutes, seconds] = [
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
+const today = new Date(2018, 2, 12);
+console.log(today.getDate())
+
+const day = today.getDate()
+console.log(day)
+
+const month = today.getMonth()
+console.log(month)
+
+const year =  today.getFullYear()
+console.log(year)
+
+const hour =  today.getHours()
+console.log(hour)
+
+const minutes =   today.getMinutes()
+console.log(minutes)
+
+const seconds =    today.getSeconds()
+console.log(seconds)
+
+const time = `${day}/${month+1}/${year} ${hour}:${minutes}:${seconds} `
+console.log(time)
+console.log(today)
+//sunday = 0
+//monday  = 1
+//tuesady = 2
+//wednesday = 3
+//thursday = 4
+// friday = 5
+// saturday = 6
+
+// january = 0
+// Fevruary = 1
+// March = 2
+
+// Decmeber = 11\
+console.log(day, month, year)
+
+
+// const time = `${day}/${month+1}/${year} ${hour}:${minutes}:${seconds} `
+//console.log(time)
+//console.log(today)
+//sunday = 0
+// monday  = 1
+// tuesady = 2
+// wednesday = 3
+// thursday = 4
+// friday = 5
+// saturday = 6
+
+// january = 0
+// Fevruary = 1
+// March = 2
+
+// Decmeber = 11
+// console.log(day, month, year2)
